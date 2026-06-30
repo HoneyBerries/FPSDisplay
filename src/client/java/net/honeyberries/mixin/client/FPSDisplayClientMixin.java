91a700eab20b309fecfddae05309a6f7bc252694
Henry Ng <henry.rainbowfish@gmail.com>
2026-06-29 22:42:18 -0700

    Patch the code to work for 26.2

--- src/client/java/net/honeyberries/mixin/client/FPSDisplayClientMixin.java
+++ src/client/java/net/honeyberries/mixin/client/FPSDisplayClientMixin.java
@@ -2,18 +2,16 @@ package net.honeyberries.mixin.client;
 
 import net.honeyberries.FPSRenderer;
 import net.minecraft.client.DeltaTracker;
-import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.Hud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Gui.class)
+@Mixin(Hud.class)
 public class FPSDisplayClientMixin {
 
-    // Inject at the TAIL (end) of the render method.
-    // This ensures it runs even if the game skipped the hotbar/crosshair due to spectator mode.
     @Inject(method = "extractRenderState", at = @At("TAIL"))
     public void renderFPS(GuiGraphicsExtractor graphics, DeltaTracker deltaTracker, CallbackInfo ci) {
         FPSRenderer.render(graphics, deltaTracker);
